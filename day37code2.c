505ba7876396cf85cf8141f0fe215312fb7baf70
Ojas Sachan <itsojassachan@gmail.com>
2026-09-21 13:39:35 +0530

    Add files via upload

---
+++ day37code2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int a[100][100];
+
+    scanf("%d %d", &r, &c);
+
+    // Input matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Transpose
+    for (int j = 0; j < c; j++) {
+        for (int i = 0; i < r; i++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
